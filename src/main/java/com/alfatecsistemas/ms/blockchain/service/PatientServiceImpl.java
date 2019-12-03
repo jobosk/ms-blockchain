038cd82f13356e715045414cbbe19cd91704d5cc
jbosca <jbosca@alfatecsistemas.es>
2019-12-03 16:11:04 +0100

    Prueba.

--- src/main/java/com/alfatecsistemas/ms/blockchain/service/PatientServiceImpl.java
+++ src/main/java/com/alfatecsistemas/ms/blockchain/service/PatientServiceImpl.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.web3j.abi.TypeReference;
 import org.web3j.abi.datatypes.generated.Uint256;
 import org.web3j.crypto.Credentials;
 import org.web3j.crypto.ECKeyPair;
@@ -153,7 +154,7 @@ public class PatientServiceImpl implements PatientService {
         , patientAddress
         , "getPatientId"
         , Arrays.asList()
-        , Arrays.asList(blockchainService.<Uint256>getReturnType())
+        , Arrays.asList(new TypeReference<Uint256>() {})
         , BigInteger.class
     );
   }
